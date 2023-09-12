db8732f8b8f797bd06d205b1bc61b5ee188a38af
Oscar Camacho <oscarcamacho699@gmail.com>
2023-09-11 19:08:59 -0500

    ajuste de rutas al servidor

--- servicios/productos-service.js
+++ servicios/productos-service.js
@@ -2,12 +2,12 @@ const urlDesarrollo =   `http://localhost:3000/productos`
 const urlProduccion = `https://solargeekapi.vercel.app/`
 
   
-const listarProductos = () =>fetch(`https://solargeekapi.vercel.app//productos/`)
+const listarProductos = () =>fetch(`https://solargeekapi.vercel.app/productos/`)
 .then(respuesta =>respuesta.json()).catch(err => console.log(err));
 
   
 
-const crearProducto = (nombre, precio, urlImagen, descripcion, categoria) =>fetch( `https://solargeekapi.vercel.app//productos/`,{
+const crearProducto = (nombre, precio, urlImagen, descripcion, categoria) =>fetch( `https://solargeekapi.vercel.app/productos/`,{
     method : "POST",
     headers : {
         "Content-Type":"application/json"
@@ -16,15 +16,15 @@ const crearProducto = (nombre, precio, urlImagen, descripcion, categoria) =>fetc
 
 });
 
-const eliminarProducto = (id) =>fetch(`https://solargeekapi.vercel.app//productos/${id}`,{
+const eliminarProducto = (id) =>fetch(`https://solargeekapi.vercel.app/productos/${id}`,{
     method : "DELETE"
 })
 
-const detalleProducto =  (id) =>fetch(`https://solargeekapi.vercel.app//productos/${id}`)
+const detalleProducto =  (id) =>fetch(`https://solargeekapi.vercel.app/productos/${id}`)
 .then(respuesta => respuesta.json());
 
 const editarProducto = (nombre, precio, urlImagen, descripcion, categoria, id) =>{
-return fetch(`https://solargeekapi.vercel.app//productos/${id}`,{
+return fetch(`https://solargeekapi.vercel.app/productos/${id}`,{
     method : "PUT",
     headers : {
         "Content-Type":"application/json"
